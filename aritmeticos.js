c7e47841d82204c1283aeb41ffb07784421d5167
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-08 21:03:16 -0600

    Funciones

--- aritmeticos.js
+++ aritmeticos.js
@@ -19,3 +19,23 @@ console.log(typeof numeroDos);
 console.log(numeroUno == numeroDos); // TRUE
 console.log(numeroUno === numeroDos); // ????? El triple igual va evaluar el tipo de dato y no el tipo de valor
 /* console.log(5 === 2); */
+
+if (5 < 2) {
+  /* Aquí va a ir nuestro bloque de código en caso de que se cumpla una condición */
+  alert("El 5 es mayor");
+} else {
+  alert("El 5 no es mayor");
+}
+
+let edad = prompt("Ingresa tu edad");
+
+if (edad == 18) {
+  /* Alerting the user that they are of age. */
+  alert("Eres mayor de edad 🥳");
+} else if (edad === 17) {
+  alert("Tienes 17 años");
+} else if (edad == 15 || edad == 16) {
+  alert("Tienes entre 15 y 16, estás chavo");
+} else if (edad == 30) {
+  alert("Estás chavorruco");
+}
---
+++ funcion.js
@@ -0,0 +1,9 @@
+let teclaIngresada = prompt("Aprieta la letra a si quieres ver la función");
+
+function saludar() {
+  alert("Nos vemos en la próxima");
+}
+
+if (teclaIngresada == "a" || teclaIngresada == "A") {
+  saludar();
+}
