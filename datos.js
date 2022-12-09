d8b039aa9875a1efe83c81d3f3d97ca651e6b48f
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-08 20:19:59 -0600

    Uso de datos en JS

--- datos.js
+++ datos.js
@@ -1,1 +1,27 @@
-let myName = "Kevin";
+let myName = "Kevin"; //Estoy guardando un dato de tipo string
+let myAge = 27; //Esta variable guarda un dato de tipo númerico
+let isStudent = false; // Esta variable guarda un dato booleano
+let isTeacher = true; // Esta variable guadar un dato booleano
+let currentYear = null; // Aquí no estoy especificando un valor en concreto
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isTeacher);
+console.log(currentYear);
+/* console.log(random); */
+
+console.log(
+  `Mi nombre es ${myName} y tengo ${myAge} años y ¿soy estudiante? ${isStudent}`
+);
+
+let aboutMe = {
+  name: "Kevin",
+  lastName: "Luciano",
+};
+
+console.log(aboutMe);
+
+let favoriteFoods = ["Pescado", "Tacos de Pastor", "Tacos de Suadero"];
+
+console.log(favoriteFoods);
