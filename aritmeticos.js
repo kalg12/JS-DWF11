496234a12113a3893f1ff2bd51941e325954daea
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-08 20:32:41 -0600

    Uso aritmético

---
+++ aritmeticos.js
@@ -0,0 +1,10 @@
+let numberOne = prompt("Ingresa un número");
+let numberTwo = prompt("Ingresa un segundo número");
+
+let additionResult = parseInt(numberOne) + parseInt(numberTwo);
+
+alert(`La suma de ${numberOne} y ${numberTwo} es: ${additionResult}`);
+/* alert(numberOne - numberTwo);
+alert(numberOne / numberTwo);
+alert(numberOne * numberTwo);
+ */
