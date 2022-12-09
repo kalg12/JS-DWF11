04f3fbc71e2f35694db45c9e218a7f46cc98905d
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-08 20:43:51 -0600

    Ejemplos con operadores relacionales

--- aritmeticos.js
+++ aritmeticos.js
@@ -1,10 +1,21 @@
-let numberOne = prompt("Ingresa un número");
-let numberTwo = prompt("Ingresa un segundo número");
+/* let numberOne = prompt("Ingresa un número");
+let numberTwo = prompt("Ingresa un segundo número"); */
 
-let additionResult = parseInt(numberOne) + parseInt(numberTwo);
+/* let additionResult = parseInt(numberOne) + parseInt(numberTwo); */
 
-alert(`La suma de ${numberOne} y ${numberTwo} es: ${additionResult}`);
+/* alert(`La suma de ${numberOne} y ${numberTwo} es: ${additionResult}`); */
 /* alert(numberOne - numberTwo);
 alert(numberOne / numberTwo);
 alert(numberOne * numberTwo);
  */
+
+let numeroUno = "2";
+let numeroDos = 2;
+
+/* console.log(5 > 2);
+console.log(5 < 2); */
+console.log(typeof numeroUno);
+console.log(typeof numeroDos);
+console.log(numeroUno == numeroDos); // TRUE
+console.log(numeroUno === numeroDos); // ????? El triple igual va evaluar el tipo de dato y no el tipo de valor
+/* console.log(5 === 2); */
